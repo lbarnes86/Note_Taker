50d55288add1e19b3905d768221d81269cf1506c
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-28 16:13:15 -0600

    updated server.js

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+app.get("/api/notes", function(req, res){
+    res.json(db.map((note, i) => ({...note, id: i + ""})))
+});
+
+// POST `/api/notes` - Should receive a new note to save on the 
+//request body, add it to the `db.json` file, and then return the 
+//new note to the client.
+app.post("/api/notes", function(req, res) {
+    db.push(req.body);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(db, null, '\t'))
+
+    res.json(req.body);
+});
 
 
 
