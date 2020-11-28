fd24ba724225871d4a166a5a6417669266277b59
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-28 15:43:52 -0600

    updated server.js

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const db = require("./db/db.json");
 
 var PORT = process.env.PORT || 8080;
 
+var app = express();
+
+
+
+app.listen(PORT, function() {
+    console.log("APP listening on PORT: " + PORT);
+});
