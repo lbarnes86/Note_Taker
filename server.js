b4f1ffadb38c8296f183b9341d331418865f00f8
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-11-28 17:19:39 -0600

    updated server.js adding routes

--- server.js
+++ server.js
@@ -1,43 +1,19 @@
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-const db = require("./db/db.json");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//GET `/notes` - Should return the `notes.html` file.
-app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
+//calls for routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-//GET `*` - Should return the `index.html` file
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/api/notes", function(req, res){
-    res.json(db.map((note, i) => ({...note, id: i + ""})))
-});
-
-// POST `/api/notes` - Should receive a new note to save on the 
-//request body, add it to the `db.json` file, and then return the 
-//new note to the client.
-app.post("/api/notes", function(req, res) {
-    db.push(req.body);
-
-    fs.writeFileSync("./db/db.json", JSON.stringify(db, null, '\t'))
-
-    res.json(req.body);
-});
-
-
-
-app.listen(PORT, function() {
-    console.log("APP listening on PORT: " + PORT);
+app.listen(PORT, function () {
+  console.log("APP listening on PORT: " + PORT);
 });
